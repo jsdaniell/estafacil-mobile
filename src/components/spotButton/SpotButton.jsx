2a29b92df3ae6551f419f3254ad167d071569f79
jsdaniell <jose.daniell@outlook.com>
2022-05-25 19:07:59 -0300

    finish him

--- src/components/spotButton/SpotButton.jsx
+++ src/components/spotButton/SpotButton.jsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     rootView: {
         flexDirection: "column",
         height: 150,
-        width: 160,
+        width: 140,
         padding: 20,
         justifyContent: "space-evenly",
         alignItems: "center",
